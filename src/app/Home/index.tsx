aff284c6f7e78b584253662d5eba00b6c37c0115
Roberto Mazor <mazor.estudo@gmail.com>
2026-05-06 20:05:24 -0300

    Update index.tsx

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from "react-native";
 import Button from "@/components/Button";
-import Input from "@/components/input";
+import Input from "@/components/Input";
 
 export default function App() {
   return (
